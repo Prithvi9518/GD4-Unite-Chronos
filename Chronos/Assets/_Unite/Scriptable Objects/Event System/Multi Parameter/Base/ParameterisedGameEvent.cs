c70b620fc87291b933b3ee8253bcb062dd2c6459
Prithvi9518 <prithvi9518@gmail.com>
2023-11-10 16:45:14 +0000

    :hammer: Add scriptable object classes for events and listeners

---
+++ Chronos/Assets/_Unite/Scriptable Objects/Event System/Multi Parameter/Base/ParameterisedGameEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unite
+{
+    public abstract class ParameterisedGameEvent<T> : ScriptableObject
+    {
+        private HashSet<ParameterisedGameEventListener<T>> listeners = new HashSet<ParameterisedGameEventListener<T>>();
+
+        [ContextMenu("Raise Event")]
+        public virtual void Raise(T data)
+        {
+            foreach (var listener in listeners)
+            {
+                listener.OnEventRaised(data);
+            }
+        }
+
+        public void RegisterListener(ParameterisedGameEventListener<T> listener)
+        {
+            listeners.Add(listener);
+        }
+
+        public void UnregisterListener(ParameterisedGameEventListener<T> listener)
+        {
+            listeners.Remove(listener);
+        }
+    }
+}
---
+++ Chronos/Assets/_Unite/Scriptable Objects/Event System/Multi Parameter/Base/ParameterisedGameEventListener.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Unite
+{
+    public class ParameterisedGameEventListener<T> : MonoBehaviour
+    {
+        [SerializeField] private string description;
+
+        [SerializeField] private ParameterisedGameEvent<T> gameEvent;
+
+        [SerializeField] private UnityEvent<T> response;
+
+        private void OnEnable()
+        {
+            gameEvent.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.UnregisterListener(this);
+        }
+
+        public virtual void OnEventRaised(T data)
+        {
+            response?.Invoke(data);
+        }
+    }
+}
