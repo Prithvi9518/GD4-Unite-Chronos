94d14926028abbdd879ede19e2e28486cd761a87
Prithvi9518 <prithvi9518@gmail.com>
2023-12-02 14:49:41 +0000

    :sparkles: Add stat buff scriptable object to increase stat value

---
+++ Chronos/Assets/_Unite/_Scripts/BuffSystem/ScriptableObjects/StatBuff.cs
@@ -0,0 +1,21 @@
+﻿using Unite.Player;
+using Unite.StatSystem;
+using UnityEngine;
+
+namespace Unite.BuffSystem
+{
+    [CreateAssetMenu(fileName = "StatBuff", menuName = "Buffs/Stat Buff")]
+    public class StatBuff : BuffScriptableObject
+    {
+        [SerializeField]
+        private StatTypeSO statType;
+        
+        [SerializeField]
+        private StatModifier modifier;
+        
+        public override void ApplyBuff(Player.Player player)
+        {
+            player.GetComponent<PlayerStatsHandler>().AddModifier(statType, modifier);
+        }
+    }
+}
