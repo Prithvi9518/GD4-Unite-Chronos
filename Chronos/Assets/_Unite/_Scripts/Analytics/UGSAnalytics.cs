badde31c3cd6c10d5c405da25d862e4e5722de3d
StvnDugz <stvndugz@gmail.com>
2023-12-19 22:31:22 +0000

    :construction: Add custom event
    Added a level completed custom event

--- Chronos/Assets/_Unite/_Scripts/Analytics/UGSAnalytics.cs
+++ Chronos/Assets/_Unite/_Scripts/Analytics/UGSAnalytics.cs
@@ -13,11 +13,30 @@ public class UGSAnalytics : MonoBehaviour
             // Initialize Unity Services asynchronously
             await UnityServices.InitializeAsync();
             GiveConsent(); // Get user consent according to various legislations
+            LevelCompletedCustomEvent();
         }
         catch (ConsentCheckException e)
         {
             Debug.Log(e.ToString());
         }
+    }
+
+    private void LevelCompletedCustomEvent()
+    {
+        int currentLevel = Random.Range(1, 4); // Gets a random number from 1-3
+
+        // Define Custom Parameters
+        Dictionary<string, object> parameters = new Dictionary<string, object>()
+        {
+            { "levelName", "level" + currentLevel.ToString()}
+        };
+
+        // The ‘levelCompleted’ event will get cached locally
+        //and sent during the next scheduled upload, within 1 minute
+        AnalyticsService.Instance.CustomData("levelCompleted", parameters);
+
+        // You can call Events.Flush() to send the event immediately
+        AnalyticsService.Instance.Flush();
     }
 
     public void GiveConsent()
