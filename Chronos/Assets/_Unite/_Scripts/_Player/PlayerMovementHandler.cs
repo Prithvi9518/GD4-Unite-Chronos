227c59d2142acf118afcfc68f8fbb2111f3f9dae
Prithvi9518 <prithvi9518@gmail.com>
2024-01-13 09:41:21 +0000

    :hammer: Add PlayerMovementHandler script to handle move and sprint speeds

---
+++ Chronos/Assets/_Unite/_Scripts/_Player/PlayerMovementHandler.cs
@@ -0,0 +1,30 @@
+﻿using StarterAssets;
+using Unite.StatSystem;
+using UnityEngine;
+
+namespace Unite.Player
+{
+    public class PlayerMovementHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private StatTypeSO speedStat;
+        
+        [SerializeField]
+        private float sprintSpeedIncrement;
+        
+        private FirstPersonController controller;
+
+        public FirstPersonController Controller => controller;
+
+        private void Awake()
+        {
+            controller = GetComponent<FirstPersonController>();
+        }
+
+        public void UpdateSpeedValue(float newValue)
+        {
+            controller.MoveSpeed = newValue;
+            controller.SprintSpeed = controller.MoveSpeed + sprintSpeedIncrement;
+        }
+    }
+}
