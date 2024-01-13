939023133ee35d2a4bca1f8680e6874b2c80a12e
Prithvi9518 <prithvi9518@gmail.com>
2024-01-13 16:51:23 +0000

    :hammer: Add scriptable object for lab notes content

---
+++ Chronos/Assets/_Unite/_Scripts/Notes/ScriptableObjects/NoteSO.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Unite.Notes
+{
+    [CreateAssetMenu(fileName = "NoteSO", menuName = "Notes/NoteSO")]
+    public class NoteSO : ScriptableObject
+    {
+        [SerializeField]
+        private string title;
+        
+        [SerializeField]
+        [TextArea(2,20)]
+        private string content;
+
+        public string Title => title;
+        public string Content => content;
+    }
+}
