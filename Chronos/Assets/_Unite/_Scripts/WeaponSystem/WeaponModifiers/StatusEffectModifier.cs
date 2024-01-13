49c327f9cb22881fd983dde9cf0c3513283b0e52
Prithvi9518 <prithvi9518@gmail.com>
2024-01-13 13:49:39 +0000

    :hammer: Add status effect modifier for guns

---
+++ Chronos/Assets/_Unite/_Scripts/WeaponSystem/WeaponModifiers/StatusEffectModifier.cs
@@ -0,0 +1,19 @@
+﻿using Unite.StatusEffectSystem;
+
+namespace Unite.WeaponSystem.Modifiers
+{
+    [System.Serializable]
+    public class StatusEffectModifier : IGunModifier
+    {
+        private StatusEffectSO statusEffect;
+
+        public StatusEffectModifier(StatusEffectSO statusEffect)
+        {
+            this.statusEffect = statusEffect;
+        }
+        public void Apply(GunData gun)
+        {
+            gun.DamageConfig.SetStatusEffect(statusEffect);
+        }
+    }
+}
