1d104845e1ab89a6205f3ab7f6eb59bc8d6a4d40
Prithvi9518 <prithvi9518@gmail.com>
2024-01-13 16:40:17 +0000

    :sparkles: Add enemy item drop system

---
+++ Chronos/Assets/_Unite/_Scripts/ItemDropSystem/EnemyDropHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Unite.ItemDropSystem
+{
+    public class EnemyDropHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject item;
+
+        [SerializeField]
+        private Transform dropPoint;
+
+        public void DropItems()
+        {
+            int probability = Random.Range(0, 2);
+            if (probability == 0) return;
+
+            Instantiate(item, dropPoint.position, Quaternion.identity);
+        }
+    }
+}
