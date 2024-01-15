d8f7bbe2d4555377e502d5e47251f2059e165693
Prithvi9518 <prithvi9518@gmail.com>
2024-01-15 14:31:23 +0000

    :hammer: Add min and max range to enemy attack data

--- Chronos/Assets/_Unite/_Scripts/Enemies/Attacks/ScriptableObjects/Attacks/AttackData.cs
+++ Chronos/Assets/_Unite/_Scripts/Enemies/Attacks/ScriptableObjects/Attacks/AttackData.cs
@@ -12,12 +12,20 @@ namespace Unite.Enemies
 
         [SerializeField]
         protected float attackRange;
+        
+        [SerializeField]
+        protected float minAttackRange;
+        
+        [SerializeField]
+        protected float maxAttackRange;
 
         [SerializeField]
         protected float attackCooldown;
 
         public AttackName AttackName => attackName;
         public float AttackRange => attackRange;
+        public float MinAttackRange => minAttackRange;
+        public float MaxAttackRange => maxAttackRange;
         public float AttackDamage => damage;
         public float AttackCooldown => attackCooldown;
 
