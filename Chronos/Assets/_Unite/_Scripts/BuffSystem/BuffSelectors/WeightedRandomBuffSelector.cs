bc20ea552c04653fd67cfcdee5a73cf0dd7f8dc7
Prithvi9518 <prithvi9518@gmail.com>
2024-01-16 17:44:14 +0000

    :sparkles: Add weighted drop chance for buffs

---
+++ Chronos/Assets/_Unite/_Scripts/BuffSystem/BuffSelectors/WeightedRandomBuffSelector.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Unite.BuffSystem.ScriptableObjects;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Unite.BuffSystem
+{
+    public class WeightedRandomBuffSelector : BuffSelector
+    {
+        [SerializeField] 
+        private List<BuffSpawnSO> buffSpawnConfigs;
+
+        private BuffSpawnConfig[] selectableBuffConfigs;
+        private float[] weights;
+
+
+        private void Awake()
+        {
+            selectableBuffConfigs = new BuffSpawnConfig[buffSpawnConfigs.Count];
+            for(int i = 0; i < selectableBuffConfigs.Length; i++)
+            {
+                selectableBuffConfigs[i] = new BuffSpawnConfig(buffSpawnConfigs[i]);
+            }
+            
+            weights = new float[selectableBuffConfigs.Length];
+        }
+
+        public override GameObject SelectBuff()
+        {
+            ResetWeights();
+
+            float value = Random.value;
+            
+            for (int i = 0; i < weights.Length; i++)
+            {
+                if (!(value < weights[i])) continue;
+                
+                BuffSpawnConfig buffSpawnConfig = selectableBuffConfigs[i];
+                if (buffSpawnConfig.CanSpawn)
+                {
+                    GameObject buffToSpawn = buffSpawnConfig.BuffSpawn.BuffPrefab;
+                    buffSpawnConfig.UpdateCanSpawn();
+                    return buffToSpawn;
+                }
+
+                value -= weights[i];
+            }
+
+            return selectableBuffConfigs[0].BuffSpawn.BuffPrefab;
+        }
+
+        private void ResetWeights()
+        {
+            float totalWeight = 0;
+            for (int i = 0; i < selectableBuffConfigs.Length; i++)
+            {
+                weights[i] = selectableBuffConfigs[i].BuffSpawn.GetWeight();
+                totalWeight += weights[i];
+            }
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] /= totalWeight;
+            }
+        }
+    }
+}
