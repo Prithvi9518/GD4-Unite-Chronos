369de7ae79535c2a1580137e95c529882d517e0c
Prithvi9518 <prithvi9518@gmail.com>
2024-01-19 15:01:51 +0000

    :hammer: Add interact text field and selection response to propagate interact text through event

---
+++ Chronos/Assets/_Unite/_Scripts/EventSystem/ScriptableObjects/Events/Multi Parameter/StringEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Unite.EventSystem
+{
+    [CreateAssetMenu(fileName = "StringEvent", menuName = "Events/StringEvent", order = 0)]
+    public class StringEvent : ParameterisedGameEvent<string>
+    {
+    }
+}
