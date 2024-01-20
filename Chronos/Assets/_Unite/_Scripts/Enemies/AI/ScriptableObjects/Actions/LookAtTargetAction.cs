010c69ab44510b530f3169c7510e6bee7c07fff8
Prithvi9518 <prithvi9518@gmail.com>
2024-01-20 18:27:32 +0000

    :sparkles: Add retreat-strafe-shoot behaviour to bat AI

---
+++ Chronos/Assets/_Unite/_Scripts/Enemies/AI/ScriptableObjects/Actions/LookAtTargetAction.cs
@@ -0,0 +1,15 @@
+﻿using Unite.StatePattern;
+using UnityEngine;
+
+namespace Unite.Enemies.AI
+{
+    [CreateAssetMenu(fileName = "LookAtTargetAction", menuName = "AI/Actions/LookAtTargetAction")]
+    public class LookAtTargetAction : Action
+    {
+        public override void ExecuteAction(BaseStateMachine baseStateMachine)
+        {
+            EnemyDetectionHandler detectionHandler = baseStateMachine.GetComponent<EnemyDetectionHandler>();
+            baseStateMachine.transform.LookAt(detectionHandler.Target);
+        }
+    }
+}
---
+++ Chronos/Assets/_Unite/_Scripts/Enemies/AI/ScriptableObjects/Conditions/PlayerOutsideAttackRangeCondition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Unite.StatePattern;
+using UnityEngine;
+
+namespace Unite.Enemies.AI
+{
+    [CreateAssetMenu(fileName = "PlayerOutsideAttackRangeCondition", menuName = "AI/Conditions/PlayerOutsideAttackRangeCondition")]
+    public class PlayerOutsideAttackRangeCondition : Condition
+    {
+        [SerializeField] 
+        private AttackData attackToCheck;
+        
+        [SerializeField]
+        private bool checkLowerRange;
+        
+        [SerializeField]
+        private bool checkUpperRange;
+        
+        public override bool VerifyCondition(BaseStateMachine baseStateMachine)
+        {
+            Enemy enemy = baseStateMachine.GetComponent<Enemy>();
+
+            Attack attack = enemy.AttackHandler.Attacks.GetValueOrDefault(attackToCheck.name, null);
+
+            if (checkLowerRange)
+            {
+                return attack.AttackData.OutsideLowerRange(enemy.transform, enemy.DetectionHandler.Target);
+            }
+
+            if (checkUpperRange)
+            {
+                return attack.AttackData.OutsideUpperRange(enemy.transform, enemy.DetectionHandler.Target);
+            }
+
+            return false;
+        }
+    }
+}
