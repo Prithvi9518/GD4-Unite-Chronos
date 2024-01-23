2a509fee289ff6bfe97479c3775e15f73feeda69
Prithvi9518 <prithvi9518@gmail.com>
2024-01-23 17:50:05 +0000

    :construction: Add events for battle zone system - for use in UI implementation

--- Chronos/Assets/_Unite/_Scripts/EventSystem/EventArguments/BattleZoneInfo.cs
+++ Chronos/Assets/_Unite/_Scripts/EventSystem/EventArguments/BattleZoneInfo.cs
@@ -3,5 +3,12 @@
     public struct BattleZoneInfo
     {
         public readonly string BattleZoneName;
+        public readonly int CurrentWave;
+
+        public BattleZoneInfo(string name, int wave)
+        {
+            BattleZoneName = name;
+            CurrentWave = wave;
+        }
     }
 }
