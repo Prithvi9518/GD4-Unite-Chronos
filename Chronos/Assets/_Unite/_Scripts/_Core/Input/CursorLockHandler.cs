7b4e5a4b23d685c30d1c4b4aa36660624023fb59
Prithvi9518 <prithvi9518@gmail.com>
2024-02-03 16:23:46 +0000

    :construction: Add cursor unlock when pausing

--- Chronos/Assets/_Unite/_Scripts/_Core/Input/CursorLockHandler.cs
+++ Chronos/Assets/_Unite/_Scripts/_Core/Input/CursorLockHandler.cs
@@ -6,6 +6,17 @@ namespace Unite.Core.Input
     {
         private void Start()
         {
+            HideAndLockCursor();
+        }
+
+        public void ShowAndUnlockCursor()
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        public void HideAndLockCursor()
+        {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
