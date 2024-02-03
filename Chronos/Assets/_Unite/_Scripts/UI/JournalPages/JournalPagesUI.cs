2d2841b8ded3b4cd7ddda6580dbe05c14d0b7414
Prithvi9518 <prithvi9518@gmail.com>
2024-02-03 15:46:07 +0000

    :bento: Add journal page images :construction: Add journal page UI skeleton

---
+++ Chronos/Assets/_Unite/_Scripts/UI/JournalPages/JournalPagesUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Unite.UI
+{
+    public class JournalPagesUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform mainPanel;
+        
+        private void Awake()
+        {
+            mainPanel.gameObject.SetActive(false);
+        }
+    }
+}
