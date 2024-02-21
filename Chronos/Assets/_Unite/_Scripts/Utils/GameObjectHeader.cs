035c8fef5adea0d40a3587393e7e125d7e958679
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2024-02-21 12:11:24 +0000

    :sparkles: Add GameObjectHeader class
    This will change the header colour of a Game Object.

---
+++ Chronos/Assets/_Unite/_Scripts/Utils/GameObjectHeader.cs
@@ -0,0 +1,58 @@
+namespace Unite.Utils
+{
+    using UnityEngine;
+    using UnityEditor;
+    
+    /// <summary>
+    /// Hierarchy Window Group Header
+    /// http://diegogiacomelli.com.br/unitytips-hierarchy-window-group-header
+    /// </summary>
+    /// <example>To create a folder make an empty with the name "*** folder name" in the hierarchy</example>
+    [InitializeOnLoad] 
+    public class GameObjectHeader : MonoBehaviour
+    {
+        private enum CategoryType
+        {
+            HierarchyOne,
+            HierarchyTwo,
+            HierarchyThree
+        }
+
+        [SerializeField]
+        private CategoryType categoryType;
+        private static readonly Color folderFillColor = Color.blue;
+        private static readonly string singleCharFolderDelimiter = "*";
+        private static readonly string folderDelimiter = $"{singleCharFolderDelimiter}{singleCharFolderDelimiter}{singleCharFolderDelimiter}";
+    
+        private GameObjectHeader()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+        }
+    
+        private void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+        {
+            var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+            if (gameObject.GetComponent<GameObjectHeader>() != null)
+            {
+                if (gameObject != null)
+                {
+                    if (categoryType == CategoryType.HierarchyOne)
+                    {
+                        EditorGUI.DrawRect(selectionRect, folderFillColor);
+                        EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace(singleCharFolderDelimiter, "").ToUpperInvariant());
+                    }
+                    else
+                    {
+                        EditorGUI.DrawRect(selectionRect, Color.yellow);
+                        EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace(singleCharFolderDelimiter, "").ToUpperInvariant());
+                    }
+
+                }
+            }
+           
+        }
+    }
+    
+    
+}
