c15dc005b2cb619bca5e16b5bb4e87d026a4244c
Prithvi9518 <prithvi9518@gmail.com>
2024-03-06 13:36:10 +0000

    :art: Add functionality to execute action only once upon interaction

---
+++ Chronos/Assets/_Unite/_Scripts/ActionSystem/ActionContext.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Unite.ActionSystem
+{
+    [System.Serializable]
+    public class ActionContext
+    {
+        [SerializeField] private ActionSO action;
+        [SerializeField] private bool doOnce;
+
+        private bool executedOnce;
+
+        public ActionSO Action => action;
+        public bool DoOnce => doOnce;
+        public bool ExecutedOnce => executedOnce;
+
+        public void RegisterFirstExecution()
+        {
+            executedOnce = true;
+        }
+    }
+}
