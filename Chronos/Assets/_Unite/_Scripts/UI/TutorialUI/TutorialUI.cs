73280bf84989386198b9fa6b99605f0cb91e7e58
Prithvi9518 <prithvi9518@gmail.com>
2024-03-26 21:04:20 +0000

    :lipstick: Add tutorial UI

---
+++ Chronos/Assets/_Unite/_Scripts/UI/TutorialUI/TutorialUI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Unite.UI
+{
+    public class TutorialUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform mainPanel;
+
+        [SerializeField]
+        private float timeBeforeShowMs;
+
+        [SerializeField]
+        private float timeBeforeHideMs;
+
+        private bool isDisplayed;
+        private Coroutine showCoroutine;
+        private Coroutine hideCoroutine;
+
+        private void Awake()
+        {
+            mainPanel.gameObject.SetActive(false);
+        }
+
+        public void ShowTutorial()
+        {
+            if (isDisplayed) return;
+            
+            if(showCoroutine != null)
+                StopCoroutine(showCoroutine);
+            if(hideCoroutine != null)
+                StopCoroutine(hideCoroutine);
+            
+            showCoroutine = StartCoroutine(ShowCoroutine());
+        }
+
+        public void HideTutorial()
+        {
+            if (!isDisplayed) return;
+            isDisplayed = false;
+            
+            mainPanel.gameObject.SetActive(false);
+            
+            if (hideCoroutine == null) return;
+            StopCoroutine(hideCoroutine);
+            hideCoroutine = null;
+        }
+
+        private IEnumerator ShowCoroutine()
+        {
+            yield return new WaitForSeconds(timeBeforeShowMs);
+            
+            isDisplayed = true;
+            mainPanel.gameObject.SetActive(true);
+
+            if (hideCoroutine != null)
+                StopCoroutine(hideCoroutine);
+            hideCoroutine = StartCoroutine(HideCoroutine());
+        }
+
+        private IEnumerator HideCoroutine()
+        {
+            yield return new WaitForSeconds(timeBeforeHideMs);
+            
+            isDisplayed = false;
+            mainPanel.gameObject.SetActive(false);
+        }
+    }
+}
