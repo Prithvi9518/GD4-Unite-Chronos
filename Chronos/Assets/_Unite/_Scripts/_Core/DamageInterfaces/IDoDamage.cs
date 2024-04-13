2170c45ea8a04b1211dc30ff7304c3b73f1bfe49
Prithvi9518 <prithvi9518@gmail.com>
2024-04-13 20:52:09 +0100

    :memo: Add comments to Core scripts and player camera scripts

--- Chronos/Assets/_Unite/_Scripts/_Core/DamageInterfaces/IDoDamage.cs
+++ Chronos/Assets/_Unite/_Scripts/_Core/DamageInterfaces/IDoDamage.cs
@@ -1,5 +1,11 @@
 namespace Unite.Core.DamageInterfaces
 {
+    /// <summary>
+    /// Used for analytics purposes to get the name of the weapon/attack dealing damage and send
+    /// the data using Unity Analytics.
+    /// Implementing the IDoDamage interface on the weapon/attack allows
+    /// the access of the name and damage type (Direct/Passive) when damage is dealt.
+    /// </summary>
     public interface IDoDamage
     {
         public string GetName();
