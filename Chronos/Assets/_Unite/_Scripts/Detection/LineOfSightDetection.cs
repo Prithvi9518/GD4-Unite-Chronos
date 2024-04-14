6c9b9f1de75fcdabb0af7634c9ad32749f78a7f7
Prithvi9518 <prithvi9518@gmail.com>
2024-04-14 14:56:58 +0100

    :memo: Add comments to detection code

--- Chronos/Assets/_Unite/_Scripts/Detection/LineOfSightDetection.cs
+++ Chronos/Assets/_Unite/_Scripts/Detection/LineOfSightDetection.cs
@@ -2,6 +2,11 @@
 
 namespace Unite.Detection
 {
+    /// <summary>
+    /// Used to detect whether a target is within the object's line of sight.
+    /// Target and obstruction masks are used to make sure the target is only seen when
+    /// the line of sight is not obstructed by any obstacles (e.g. walls, rocks, etc.)
+    /// </summary>
     public class LineOfSightDetection : MonoBehaviour, IDetectTarget
     {
         [SerializeField]
@@ -20,6 +25,18 @@ namespace Unite.Detection
         public float DetectionRadius => detectionRadius;
         public float AngleInDegrees => angleInDegrees;
         
+        /// <summary>
+        /// First, a sphere overlap is done to check whether the target is within the detected radius.
+        /// 
+        /// Second, a check is performed to see if the target is within the specified field of view.
+        /// 
+        /// Next, a raycast is sent towards the target's direction to check for any obstacles.
+        /// 
+        /// If the raycast finds no obstacles in between the object and the target, then it means the
+        /// target has been detected.
+        /// </summary>
+        /// <param name="target">Target to find</param>
+        /// <returns></returns>
         public bool IsTargetDetected(Transform target)
         {
             Collider[] results = new Collider[1];
