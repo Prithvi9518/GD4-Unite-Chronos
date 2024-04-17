981314a5427e011e77589168d6dfeae54ae250b5
Prithvi9518 <prithvi9518@gmail.com>
2024-04-17 13:51:02 +0100

    :construction:  Add skeleton code for controls prompt UI

---
+++ Chronos/Assets/_Unite/_Scripts/UI/ControlPromptUI/ControlPromptUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unite.UI
+{
+    public class ControlPromptUI : MonoBehaviour
+    {
+        [Header("Prompt Images:")]
+        [SerializeField] private Image wasdImage;
+        [SerializeField] private Image sprintImage;
+        [SerializeField] private Image jumpImage;
+        
+        [Header("Fade Out Timing Variables:")]
+        [SerializeField] private float timeBeforeFadeOutInSecs;
+        [SerializeField] private float fadeOutTimeInSecs;
+        
+        public void ShowMovementPrompt()
+        {
+        }
+
+        public void ShowSprintPrompt()
+        {
+        }
+
+        public void ShowJumpPrompt()
+        {
+        }
+
+        public void HideAllPrompts()
+        {
+        }
+    }
+}
